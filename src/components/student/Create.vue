1f46f29eb969cb2d9ffa0d380b8df1bf60e2610d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-11-28 10:54:31 +0700

    Create component

---
+++ src/components/student/Create.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { reactive } from "vue";
+
+const model = reactive({
+  student: {
+    name: "",
+    email: "",
+    phone: "",
+  },
+});
+
+const emit = defineEmits(["add-student"]);
+
+function saveStudent() {
+  emit("add-student", model.student);
+  model.student = {};
+}
+</script>
+
+<template>
+  <div class="container mt=5">
+    <div class="card">
+      <div class="card-header">
+        <h4>Add Student</h4>
+      </div>
+      <div class="card-body">
+        <div class="mb-3">
+          <label for="name">Name</label>
+          <input id="name" v-model="model.student.name" type="text" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label for="email">Email</label>
+          <input id="email" v-model="model.student.email" type="text" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label for="phone">Phone</label>
+          <input id="phone" v-model="model.student.phone" type="text" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <button type="button" @click="saveStudent">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Create from "@/components/student/Create.vue";
 import List from "@/components/student/List.vue";
 import { ref } from "vue";
 
@@ -6,10 +7,17 @@ const students = ref([
   { id: 1, name: "A", email: "a@gmail.com", phone: "111111" },
   { id: 2, name: "B", email: "b@gmail.com", phone: "222222" },
 ]);
+
+const addStudent = (student) => {
+  student.id = students.value.length + 1;
+
+  students.value.push(student);
+};
 </script>
 
 <template>
   <div class="container">
+    <Create @add-student="addStudent"></Create>
     <List :students="students"></List>
   </div>
 </template>
